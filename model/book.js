7a0fa49eb7bd195a5c9311b3aeca5180dc2f508d
Muhammad bin Tanveer <mbtgood91@gmail.com>
2022-11-28 01:09:38 +0500

    'updates'

--- model/book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-
-const booksSchema = new mongoose.Schema({
-  book_name: { type: String, default: null },
-  book_title: { type: String, default: null },
-});
-
-module.exports = mongoose.model("book", booksSchema);
