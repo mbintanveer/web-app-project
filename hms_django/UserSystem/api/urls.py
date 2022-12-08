8df55a5cda87c24b9b861304c403996c5f842efe
Muhammad bin Tanveer <mbtgood91@gmail.com>
2022-12-08 10:57:16 +0500

    'changes'

--- hms_django/UserSystem/api/urls.py
+++ hms_django/UserSystem/api/urls.py
@@ -1,10 +1,18 @@
 from  django.urls import path
-from .views import PatientSignupView, DoctorSignupView
+from .views import PatientSignupView, DoctorSignupView,CustomAuthToken, LogoutView, PatientOnlyView, DoctorOnlyView
 
 urlpatterns = [
 
     path('signup/patient/', PatientSignupView.as_view()),
     path('signup/doctor/', DoctorSignupView.as_view()),
+
+    #LOGIN/LOGOUT
+    path('login/', CustomAuthToken.as_view(), name='auth_token'),
+    path('logout/', LogoutView.as_view()),
+
+    path('patient/dashboard', PatientOnlyView.as_view()),
+    path('doctor/dashboard', DoctorOnlyView.as_view()),
+
 
 
 ]
--- hms_django/UserSystem/api/views.py
+++ hms_django/UserSystem/api/views.py
@@ -1,8 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics,permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from UserSystem.models import User
 from .serializers import UserSerializer,PatientSignupSerializer, DoctorSignupSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from .permissions import IsPatientUser, IsDoctorUser
 
 class PatientSignupView(generics.GenericAPIView):
     serializer_class=PatientSignupSerializer
@@ -28,3 +31,35 @@ class DoctorSignupView(generics.GenericAPIView):
         return Response ({"user": UserSerializer(user, context= self.get_serializer_context()).data,
         "token": Token.objects.get(user=user).key, 
         "message": "Account created successfully"})
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer=self.serializer_class(data=request.data,
+        context = {'request':request})
+        serializer.is_valid(raise_exception=True)
+        user=serializer.validated_data['user']
+        token, created=Token.objects.get_or_create(user=user)
+        return Response({
+        'token': token.key,
+        'user_id': user.pk,
+        'is_patient': user.is_patient,
+        })
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        request.auth.delete()
+        return Response(status=status.HTTP_200_OK)
+
+class PatientOnlyView(generics.RetrieveAPIView):
+    permission_classes=[permissions.IsAuthenticated&IsPatientUser]
+    serializer_class=UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class DoctorOnlyView(generics.RetrieveAPIView):
+    permission_classes=[permissions.IsAuthenticated&IsDoctorUser]
+    serializer_class=UserSerializer
+
+    def get_object(self):
+        return self.request.user
